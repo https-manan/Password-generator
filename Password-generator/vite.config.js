3da58a9dadf89eacbc2e4f8b1a9d7c5d9c90657d
https-manan <166443202+https-manan@users.noreply.github.com>
2025-07-22 18:23:12 +0530

    Update vite.config.js

--- Password-generator/vite.config.js
+++ Password-generator/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['ec2-13-60-30-16.eu-north-1.compute.amazonaws.com']
+  }
 })
