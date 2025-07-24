e8c1fda1cb3cae9b4f525dbf6a1eed3892745fd9
https-manan <166443202+https-manan@users.noreply.github.com>
2025-07-24 18:37:10 +0530

    Update vite.config.js

--- Password-generator/vite.config.js
+++ Password-generator/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
-      'ec2-13-60-30-16.eu-north-1.compute.amazonaws.com',
-      'manandev.duckdns.org'
+      'manandev.duckdns.org',
     ]
   }
 });
